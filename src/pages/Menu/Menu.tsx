4c5afc018827b5f6f850b7d7e20c076ddcf652de
aleksandr258 <aleksandralekseev258@gmail.com>
2026-08-21 00:10:22 +0900

    поиск в меню

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -5,15 +5,22 @@ import { ProductList } from './MenuList/MenuList';
 import { useGetProductsQuery } from '../../api/baseApi';
 import { isErrorWithMessage } from '../../api/apiError';
 import { ProductCardSkeleton } from '../../components/ProductCard/ProductCardSkeleton';
-
+import { useState } from 'react';
 
 function Menu(){
+	const [ searchTerm, setSearchTerm ] = useState<string>('');
 	const { data, isLoading, error } = useGetProductsQuery();
+
+	const displayedProducts = searchTerm 
+		? data?.filter(product => product.name.toLocaleLowerCase().includes(searchTerm.toLowerCase())
+			|| product.ingredients.some(ingridient => ingridient.toLowerCase().includes(searchTerm.toLowerCase())))
+		: data;
+
 
 	return<>
 		<div className={styles['head']}>
 			<Headling>Меню</Headling>
-			<Search placeholder='Введите блюдо или состав'/>
+			<Search placeholder='Введите блюдо или состав' onChange={(e) => setSearchTerm(e.target.value)}/>
 		</div>
 		<div>
 			{isLoading && <div className='flex items-center justify-center flex-wrap gap-11'>
@@ -23,7 +30,11 @@ function Menu(){
 				<ProductCardSkeleton/>
 			</div>}
 			{isErrorWithMessage(error) && <>{error.message}</>}
-			{data && <ProductList products={data}/>}
+			{displayedProducts && (
+				displayedProducts.length > 0
+					? <ProductList products={displayedProducts}/>
+					:	<div>Ничего не найдено</div>
+			)}
 		</div>
 	</>;
 }
